dbf08cc0920cc44a771cd3f18da8a419d69605b1
alaeddinehamroun <alaeddinehamroun@insat.u-carthage.tn>
2023-06-05 08:54:46 +0100

    fixed joblib error

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
 import traceback
-
+import joblib
 import pandas as pd
-from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
